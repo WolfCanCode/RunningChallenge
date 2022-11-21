9398751a40d445958c2af6b134018a46d14f7e72
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2022-11-21 09:24:28 +0700

    fix: layout mobile fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useWindowSize } from "hooks/useWindowSize";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { UserContextWrapper, useUserContext } from "../context/user";
@@ -10,20 +11,24 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { width, height } = useWindowSize();
+
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, [width, height]);
   return (
     <html lang="en">
       <head />
-      <body>
-        <div className="h-screen w-screen bg-lead relative flex flex-col justify-center">
-          <div className="
+      <body className="lg:py-edges w-screen bg-lead relative flex flex-col justify-center">
+        <div className="
             lg:h-[932px] lg:w-[430px] lg:max-h-[932px] lg:max-w-[430px] h-full w-full  overflow-hidden 
-            bg-coronation m-auto relative border-coronation border-solid border-2
-            rounded-xl">
-            {/* Manages user token */}
-            <UserContextWrapper>
-              {children}
-            </UserContextWrapper>
-          </div>
+            bg-coronation m-auto relative lg:border-coronation lg:border-solid lg:border-2
+            lg:rounded-xl">
+          {/* Manages user token */}
+          <UserContextWrapper>
+            {children}
+          </UserContextWrapper>
         </div>
       </body>
     </html>
